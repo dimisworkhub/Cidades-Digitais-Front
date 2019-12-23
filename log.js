cb5ce5c71fca670e350df2bf0647706cea7490b6
dimisworkhub <58261799+dimisworkhub@users.noreply.github.com>
2019-12-23 11:20:34 -0300

    Add files via upload

---
+++ log.js
@@ -0,0 +1,18 @@
+    	var info = {login: "", senha: ""};
+        function changer(){
+
+        var x = document.getElementById("submitLogin") ;
+		info.login = x.value;
+
+		var y = document.getElementById("submitSenha") ;
+		info.senha = y.value;
+		}
+
+		function envio(){   
+		var objetivo = JSON.stringify(info);
+		
+		fetch("read/usuario/login", {
+			method: "POST",
+			body: objetivo
+		});
+		}	
