1c602332d81481447c07c8e5a83f03b48df43116
danielbenoll <danielbenoll@hotmail.com>
2020-02-05 10:01:50 -0400

    O menu funciona de forma independente. O aquivo  se chama , o codigo do menu esta na pasta

---
+++ Js/menu.js
@@ -0,0 +1,27 @@
+document.write(`
+			<div class="container-fluid">
+				<header class="cd-header"><!--Barra Menu-->
+		
+            <div id="cd-logo">
+				<a href="#0"><img src="img/cd-logo.svg" alt="Logo"></a>
+			</div>
+				 <a class="cd-menu-trigger" href="#main-nav">Menu<span></span></a>
+				</header>
+			</div>
+
+			<div class="cd-blurred-bg"></div>
+			<nav id="main-nav">
+					<ul>
+					<li><a href="./home.html"><span>Home</span></a></li>	
+                    <li><a href="./entidade.html"><span>Entidade</span></a></li>
+                    <li><a href="#0"><span>CD</span></a></li>
+                    <li><a href="#0"><span>About</span></a></li>
+                    <li><a href="#0"><span>Contact us</span></a></li>
+					</ul>
+					<a href="#0" class="cd-close-menu">Close<span></span></a>
+				</nav>
+		</section> <!-- cd-intro -->
+
+		
+	
+	<div class="cd-shadow-layer"></div>`);
