457eebe75d97ab8792e39dfde2f91c9f9092086b
dimisworkhub <dimitrius_cardoso@hotmail.com>
2020-02-14 08:40:41 -0300

    atualizando opções de municipios

--- Js/gerenciaEntidade.js
+++ Js/gerenciaEntidade.js
@@ -46,9 +46,6 @@ function enviar(){
     headers: {'Authorization': 'Bearer ' + meuToken},
   }).then(function(response){
 
-    //checar o status do pedido
-    console.log(response);
-
     //tratamento dos erros
     if(response.status == 200){
       window.location.replace("./entidade.html");
@@ -95,75 +92,7 @@ function enviar(){
     else{
       alert("ERRO DESCONHECIDO");
     }
-    return response.json().then(function(json){
-    console.log(json);
-      });
-    });
-}
-
-/*
-window.onload=function(){
-
-  fetch('http://localhost:8080/read/municipio', {
-    method: 'GET',
-    headers: {'Authorization': 'Bearer ' + meuToken},
-  }).then(function(response){
-
-    //checar o status do pedido
-    console.log(response);
-
-    //tratamento dos erros
-    if(response.status == 200){
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status ==201){
-      alert("Usuário criado com sucesso");
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status == 202){
-      alert("Login efetivado com sucesso");
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status ==204){
-      alert("Apagado com sucesso.");
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status ==400){
-      window.location.replace("./errors/400.html");
-    }
-    else if(response.status ==401){
-      window.location.replace("./errors/401.html");
-    }
-    else if(response.status ==403){
-      window.location.replace("./errors/403.html");
-    }
-    else if(response.status ==404){
-      window.location.replace("./errors/404.html");
-    }
-    else if(response.status ==409){
-      alert("Erro: Usuário já existente.");
-    }
-    else if(response.status == 412){
-      alert("Erro: Informação colocada é incorreta.");
-    }
-    else if(response.status == 422){
-      alert("Erro: Usuário ou senha inválidos.");
-    }
-    else if(response.status == 500){
-      window.location.replace("./errors/500.html");
-    }
-    else if(response.status == 504){
-      window.location.replace("./errors/504.html");
-    }
-    else{
-      alert("ERRO DESCONHECIDO");
-    }
-    return response.json().then(function(json){
-      for (let i=0;i<json.length;i++) {
-        x += "<option>" + response.uf[i] + "</option>"
-      }
-        document.getElementById("estados").innerHTML = x;
-      });
+    return console.log(response);
     });
 }
 
@@ -176,21 +105,13 @@ window.onload=function(){
 
     //checar o status do pedido
     console.log(response);
-
+  
     //tratamento dos erros
     if(response.status == 200){
-      window.location.replace("./entidade.html");
+      console.log("200 ok.");
     }
     else if(response.status ==201){
       alert("Usuário criado com sucesso");
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status == 202){
-      alert("Login efetivado com sucesso");
-      window.location.replace("./entidade.html");
-    }
-    else if(response.status ==204){
-      alert("Apagado com sucesso.");
       window.location.replace("./entidade.html");
     }
     else if(response.status ==400){
@@ -224,10 +145,19 @@ window.onload=function(){
       alert("ERRO DESCONHECIDO");
     }
     return response.json().then(function(json){
-      for (let i=0;i<json.length;i++){
-        x += "<option>" + response.nome_municipio[i] + "</option>"
+      let x,y,i;
+      x = "<option>" + json["0"].uf + "</option>"
+      for (i=0;i<json.length;i++) {
+        x += "<option>" + json[i].uf + "</option>"
       }
-        document.getElementById("municipios").innerHTML = x;
-      });
+        document.getElementById("uf").innerHTML = x;
+
+      y = "<option>" + json["0"].nome_municipio + "</option>"
+      for (i=0;i<json.length;i++){
+        y += "<option>" + json[i].nome_municipio + "</option>"
+      }
+        document.getElementById("nomeMun").innerHTML = y;
+      })
+
     });
-}*/
+}
