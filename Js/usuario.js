cad10972905a6efc8a3d6ca0474b45e4d03f3e70
danielbenoll <danielbenoll@hotmail.com>
2020-02-20 11:54:14 -0400

    testes para modulos

--- Js/usuario.js
+++ Js/usuario.js
@@ -3,9 +3,10 @@ let cod_usuarioQuery = [];
 //pega o token do login
 let meuToken = localStorage.getItem("token");
 
+let uModulos=[];
 
 //tratamento de erros
-function erros(value){
+function erros(value) {
   if (value == 400) {
     window.location.replace("./errors/400.html");
   } else if (value == 401) {
@@ -31,25 +32,25 @@ function erros(value){
 
 
 window.onload = function () {
-    //função fetch para mandar
-    fetch('http://localhost:8080/read/usuario', {
-        method: 'GET',
-        headers: {
-          'Authorization': 'Bearer ' + meuToken
-        },
-      }).then(function (response) {
-          //tratamento dos erros
-          if (response.status == 200) {
-            console.log("ok");
-          } else {
-            erros(response.status);
-          }
-          //pegar o json que possui a tabela
-          return response.json().then(function (json) {
-            console.log(json);
-            console.log(response.text);
+  //função fetch para mandar
+  fetch('http://localhost:8080/read/usuario', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + meuToken
+    },
+  }).then(function (response) {
+    //tratamento dos erros
+    if (response.status == 200) {
+      console.log("ok");
+    } else {
+      erros(response.status);
+    }
+    //pegar o json que possui a tabela
+    return response.json().then(function (json) {
+      console.log(json);
+      console.log(response.text);
 
-            let tabela = (`<thead style="background: #4b5366; color:white; font-size:15px">
+      let tabela = (`<thead style="background: #4b5366; color:white; font-size:15px">
           <tr>
           <th> <span class="custom-checkbox">
           <input type="checkbox" id="selectAll">
@@ -63,30 +64,30 @@ window.onload = function () {
           <th scope="col">Opções</th>
           </tr>
           </thead>`);
-            tabela += (`<tbody> <tr>`);
+      tabela += (`<tbody> <tr>`);
 
 
 
-            for (let i = 0; i < json.length; i++) {
-              cod_usuarioQuery[i] = json[i]["cod_usuario"];
-              tabela += (`<td>
+      for (let i = 0; i < json.length; i++) {
+        cod_usuarioQuery[i] = json[i]["cod_usuario"];
+        tabela += (`<td>
               <span class="custom-checkbox">
               <input type="checkbox" id="checkbox1" name="o
               console.log(response.text);ptions[]" value="1">
               <label for="checkbox1"></label>
               </span>
               </td>`);
-              tabela += (`<td>`);
-              tabela += json[i]["cod_usuario"];
-              tabela += (`</td> <td>`);
-              tabela += json[i]["nome"]
-              tabela += (`</td> <td>`);
-              tabela += json[i]["email"]
-              tabela += (`</td> <td>`);
-              tabela += json[i]["status"]
-              tabela += (`</td> <td>`);
-              tabela += json[i]["login"]
-              tabela += (`</td> <td> 
+        tabela += (`<td>`);
+        tabela += json[i]["cod_usuario"];
+        tabela += (`</td> <td>`);
+        tabela += json[i]["nome"]
+        tabela += (`</td> <td>`);
+        tabela += json[i]["email"]
+        tabela += (`</td> <td>`);
+        tabela += json[i]["status"]
+        tabela += (`</td> <td>`);
+        tabela += json[i]["login"]
+        tabela += (`</td> <td> 
               <span class="d-flex">
               <button onclick="editarUsuario(` + i + `)" class="btn btn-success">
               <i class="material-icons"data-toggle="tooltip" title="Edit">&#xE254;</i>
@@ -95,61 +96,64 @@ window.onload = function () {
               <i class="material-icons"data-toggle="tooltip" title="Delete">&#xE872;</i>
               </button> 
               </span> </td>`);
-              tabela += (`</tr> <tr>`);
-            }
+        tabela += (`</tr> <tr>`);
+      }
 
 
-            tabela += (`</tr> </tbody>`);
-            document.getElementById("tabela").innerHTML = tabela;
+      tabela += (`</tr> </tbody>`);
+      document.getElementById("tabela").innerHTML = tabela;
 
-            $(document).ready(function () {
-              // Select/Deselect checkboxes
-              var checkbox = $('table tbody input[type="checkbox"]');
-              $("#selectAll").click(function () {
-                if (this.checked) {
-                  checkbox.each(function () {
-                    this.checked = true;
-                  });
-                } else {
-                  checkbox.each(function () {
-                    this.checked = false;
-                  });
-                }
-              });
-              checkbox.click(function () {
-                if (!this.checked) {
-                  $("#selectAll").prop("checked", false);
-                }
-              });
+      $(document).ready(function () {
+        // Select/Deselect checkboxes
+        let checkbox = $('table tbody input[type="checkbox"]');
+        $("#selectAll").click(function () {
+          if (this.checked) {
+            checkbox.each(function () {
+              this.checked = true;
             });
+          } else {
+            checkbox.each(function () {
+              this.checked = false;
+            });
+          }
+        });
+        checkbox.click(function () {
+          if (!this.checked) {
+            $("#selectAll").prop("checked", false);
+          }
+        })
+      })
+
+    });
+  });
 
 
 
-    //Fazer Tabela para Modulos
+  //Fazer Tabela para Modulos
 
-    //função fetch para mandar itens 
-    //mudar para modulo quando possivel e se necessario
-    fetch('http://localhost:8080/read/entidade', {
-       method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + meuToken
-      },
-        }).then(function (response) {
-             //tratamento dos erros
-            if (response.status == 200) {
-               console.log("ok");
-             } else if (response.status == 201) {
-              console.log("Entidade criada com sucesso");
-            } else if (response.status == 204) {
-              console.log("Apagado com sucesso.");
-             } else {
-               erros(response.status);
-            }
-             //pegar o json que possui a tabela
-            return response.json().then(function (json) {
-              console.log(json);
-              let cod_moduloQuery = [];
-              let tabelaMod = (`<thead style="background: #4b5366; color:white; font-size:15px">
+  //função fetch para mandar itens 
+  //mudar para modulo quando possivel e se necessario
+  fetch('http://localhost:8080/read/modulo', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + meuToken
+    },
+  }).then(function (response) {
+    //tratamento dos erros
+    if (response.status == 200) {
+      console.log("ok");
+    } else if (response.status == 201) {
+      console.log("modulo criada com sucesso");
+    } else if (response.status == 204) {
+      console.log("Apagado com sucesso.");
+    } else {
+      erros(response.status);
+    }
+    //pegar o json que possui a tabela
+    return response.json().then(function (json) {
+      console.log(json);
+      let cod_moduloQuery = [];
+      let tabelaMod = (`<thead style="background: #4b5366; color:white; font-size:15px">
               <tr>
               <th> <span class="custom-checkbox">
               <input type="checkbox" id="selectAll">
@@ -161,39 +165,62 @@ window.onload = function () {
               <th scope="col">Ação</th>
               </tr>
               </thead>`);
-              tabelaMod += (`<tbody> <tr>`);
+      tabelaMod += (`<tbody> <tr>`);
 
 
 
-              for (let i = 0; i < json.length; i++) {
-                cod_moduloQuery[i] = json[i]["cod_modulo"];
-                tabelaMod += (`<td>
+      for (let i = 0; i < json.length; i++) {
+        cod_moduloQuery[i] = json[i]["cod_modulo"];
+        tabelaMod += (`<td>
               <span class="custom-checkbox">
-              <input type="checkbox" id="checkbox1" name="o
-              console.log(response.text);ptions[]" value="1">
+              <input onclick="modulos()" type="checkbox" id="checkbox1" name="o
+              console.log(response.text);ptions[]" value="`+json[i]["cod_modulo"]+`">
               <label for="checkbox1"></label>
               </span>
               </td>`);
-                tabelaMod += (`<td>`);
-                tabelaMod += json[i]["cod_modulo"];
-                tabelaMod += (`</td> <td>`);
-                tabelaMod += json[i]["categoria_1"]
-                tabelaMod += (`</td> <td>`);
-                tabelaMod += json[i]["categoria_2"]
-                tabelaMod += (`</td> <td>`);
-                tabelaMod += json[i]["categoria_3"]
-                tabelaMod += (`</td>`);
-                tabelaMod += (`</tr> <tr>`);
-              }
+        tabelaMod += (`<td>`);
+        tabelaMod += json[i]["cod_modulo"];
+        tabelaMod += (`</td> <td>`);
+        tabelaMod += json[i]["categoria_1"]
+        tabelaMod += (`</td> <td>`);
+        tabelaMod += json[i]["categoria_2"]
+        tabelaMod += (`</td> <td>`);
+        tabelaMod += json[i]["categoria_3"]
+        tabelaMod += (`</td>`);
+        tabelaMod += (`</tr> <tr>`);
+
+        uModulos[i]=
+      }
 
 
-              tabelaMod += (`</tr> </tbody>`);
-              document.getElementById("tabelaMod").innerHTML = tabelaMod;
+      tabelaMod += (`</tr> </tbody>`);
+      document.getElementById("tabelaMod").innerHTML = tabelaMod;
+
+      $(document).ready(function () {
+        // Select/Deselect checkboxes
+        let checkbox = $('table tbody input[type="checkbox"]');
+        $("#selectAll").click(function () {
+          if (this.checked) {
+            checkbox.each(function () {
+              this.checked = true;
             });
-          });
+          } else {
+            checkbox.each(function () {
+              this.checked = false;
+            });
+          }
         });
-      });
+        checkbox.click(function () {
+          if (!this.checked) {
+            $("#selectAll").prop("checked", false);
+          }
+        })
+      })
+
+    });
+  });
 }
+
 function editarUsuario(valor) {
   localStorage.setItem("cod_usuario", cod_usuarioQuery[valor]);
   window.location.href = "./gerenciaUsuario.html";
@@ -201,7 +228,7 @@ function editarUsuario(valor) {
 
 
 //Fazer Usuário
-var info = {
+let info = {
   "nome": "",
   "email": "",
   "login": "",
@@ -209,13 +236,13 @@ var info = {
 };
 
 function changer() {
-  var a = document.getElementById("nome");
+  let a = document.getElementById("nome");
   info.nome = a.value;
-  var b = document.getElementById("email");
+  let b = document.getElementById("email");
   info.email = b.value;
-  var c = document.getElementById("login");
+  let c = document.getElementById("login");
   info.login = c.value;
-  var d = document.getElementById("senha");
+  let d = document.getElementById("senha");
   info.senha = d.value;
 }
 
@@ -241,7 +268,7 @@ function enviar() {
       alert("Usuário criado com sucesso");
     } else {
       erros(response.status);
-   }
+    }
     return response.json().then(function (json) {
       console.log(json);
     });
@@ -253,20 +280,20 @@ function apagarUsuario(valor) {
   let corpo = JSON.stringify(info);
 
   //função fetch para mandar
- fetch('http://localhost:8080/read/usuario/' + cod_usuarioQuery[valor], {
-   method: 'DELETE',
-   headers: {
-     'Authorization': 'Bearer ' + meuToken
-   },
- }).then(function (response) {
-   //checar o status do pedido
-   console.log(response);
-   //tratamento dos erros
-   if (response.status == 204) {
-     alert("Apagado com sucesso.");
-     window.location.replace("./usuario.html");
-   } else {
-     erros(response.status);
-   }
- });
+  fetch('http://localhost:8080/read/usuario/' + cod_usuarioQuery[valor], {
+    method: 'DELETE',
+    headers: {
+      'Authorization': 'Bearer ' + meuToken
+    },
+  }).then(function (response) {
+    //checar o status do pedido
+    console.log(response);
+    //tratamento dos erros
+    if (response.status == 204) {
+      alert("Apagado com sucesso.");
+      window.location.replace("./usuario.html");
+    } else {
+      erros(response.status);
+    }
+  });
 }
