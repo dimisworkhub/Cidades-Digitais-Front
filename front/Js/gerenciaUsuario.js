35eaf11b478a3fe174860e30451d8b82e37289ee
dimitrius <dimitrius_cardoso@hotmail.com>
2020-07-23 15:06:57 -0300

    javascript global criado

--- front/Js/gerenciaUsuario.js
+++ front/Js/gerenciaUsuario.js
@@ -1,47 +1,33 @@
-let info = {
-  "cod_usuario": "",
-  "nome": "",
-  "email": "",
-  "login": "",
-  "status": "",
-  "senha": "",
-};
 
 //pega o valor do usuario logado
 let codigoLogado = localStorage.getItem("codigoLogado");
 
-//pega os valores corretos das variaveis
-let meuCodigo = localStorage.getItem("cod_usuario");
-let meuNome = localStorage.getItem("nome");
-let meuEmail = localStorage.getItem("email");
-let meuLogin = localStorage.getItem("login");
-let meuStatus = localStorage.getItem("status");
-let meuSenha = localStorage.getItem("senha");
-
 
 window.onload = function () {
   // inserindo os valores no html
-  document.getElementById("cod_usuario").value = meuCodigo;
-  document.getElementById("nome").value = meuNome;
-  document.getElementById("email").value = meuEmail;
-  document.getElementById("login").value = meuLogin;
-  document.getElementById("status").value = meuStatus;
-  document.getElementById("senha").value = meuSenha;
+  document.getElementById("cod_usuario").value = localStorage.getItem("cod_usuario");
+  document.getElementById("nome").value = localStorage.getItem("nome");
+  document.getElementById("email").value = localStorage.getItem("email");
+  document.getElementById("login").value = localStorage.getItem("login");
+  document.getElementById("status").value = localStorage.getItem("status");
+  document.getElementById("senha").value = localStorage.getItem("senha");
 }
 
 
 function enviar() {
 
-  info.cod_usuario = parseInt(document.getElementById("cod_usuario").value);
-  info.nome = document.getElementById("nome").value;
-  info.email = document.getElementById("email").value;
-  info.login = document.getElementById("login").value;
-  info.status = document.getElementById("status").value;
-  info.senha = document.getElementById("senha").value;
+  let info = {
+    "cod_usuario": parseInt(document.getElementById("cod_usuario").value),
+    "nome": document.getElementById("nome").value,
+    "email": document.getElementById("email").value,
+    "login": document.getElementById("login").value,
+    "status": document.getElementById("status").value,
+    "senha": document.getElementById("senha").value,
+  };
 
   //transforma as informações do token em json
   let corpo = JSON.stringify(info);
-  console.log(corpo);
+  //console.log(corpo);
   //função fetch para mandar
   fetch(servidor + 'read/usuario/' + parseInt(codigoLogado), {
     method: 'PUT',
@@ -52,14 +38,14 @@ function enviar() {
   }).then(function (response) {
 
     //checar o status do pedido
-    console.log(response.status);
+    //console.log(response.status);
 
     //tratamento dos erros
     if (response.status == 200 || response.status == 202) {
       response.json().then(function (json) {
-        console.log(json);
-      });
+        //console.log(json);
       window.location.replace("./usuario.html");
+      });
     } else {
       erros(response.status);
     }
