361d47cc43707c9d1205ac071b36c209b02bffe3
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-09-03 08:36:13 -0300

    adicionado funcoes de SELECT e de DELETE, arg SELECT (cod_ibge, data), arg DELETE (cod_ibge, data, cod_assunto).

--- api/models/uacomAssunto.go
+++ api/models/uacomAssunto.go
@@ -26,3 +26,36 @@ func (uacomAssunto *UacomAssunto) SaveUacomAssunto(db *gorm.DB) (*UacomAssunto, 
 
 	return uacomAssunto, err
 }
+
+/*  =========================
+	FUNCAO LISTAR UACOM ASSUNTO
+=========================  */
+
+func (uacomAssunto *UacomAssunto) FindAllUacomAssunto(db *gorm.DB, codIbge uint32, data string) (*[]UacomAssunto, error) {
+
+	allUacomAssunto := []UacomAssunto{}
+
+	//	Busca um elemento no banco de dados a partir de sua chave primaria
+	err := db.Debug().Select("uacom_assunto.*").
+		Where("cod_ibge = ? AND data = ?", codIbge, data).
+		Order("uacom_assunto.cod_assunto").
+		Scan(&allUacomAssunto).Error
+
+	if err != nil {
+		return &[]UacomAssunto{}, err
+	}
+
+	return &allUacomAssunto, err
+}
+
+/*  =========================
+	FUNCAO DELETAR UACOM ASSUNTO
+=========================  */
+
+func (uacomAssunto *UacomAssunto) DeleteUacomAssunto(db *gorm.DB, codIbge uint32, data string, codAssunto uint32) error {
+
+	//	Deleta um elemento contido no banco de dados a partir de sua chave primaria
+	db = db.Debug().Model(&UacomAssunto{}).Where("cod_ibge = ? AND data = ? AND cod_assunto = ?", codIbge, data, codAssunto).Take(&Uacom{}).Delete(&UacomAssunto{})
+
+	return db.Error
+}
