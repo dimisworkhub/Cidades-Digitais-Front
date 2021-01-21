218b7d7c268e5fa4e2bee3c4b6bc6391cc26c90e
alexandreps1123 <alexandre.ps1123@gmail.com>
2021-01-21 11:05:53 -0300

    Criado as funçoes de GetByID, Update e Delete.

--- api/control/modulo_control.go
+++ api/control/modulo_control.go
@@ -12,6 +12,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 /*  =========================
@@ -79,6 +82,42 @@ func (server *Server) CreateModulo(w http.ResponseWriter, r *http.Request) {
 }
 
 /*  =========================
+	FUNCAO LISTAR MODULO POR ID
+=========================  */
+
+func (server *Server) GetModuloByID(w http.ResponseWriter, r *http.Request) {
+
+	//	Autorizacao de Modulo
+	err := config.AuthMod(w, r, 11002)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
+		return
+	}
+
+	//	Vars retorna as variaveis de rota
+	vars := mux.Vars(r)
+
+	//	codModulo armazena a chave primaria da tabela modulo
+	codModulo, err := strconv.ParseUint(vars["cod_modulo"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
+		return
+	}
+
+	modulo := models.Modulo{}
+
+	//	moduloGotten recebe o dado buscado no banco de dados
+	moduloGotten, err := modulo.FindModuloByID(server.DB, uint32(codModulo))
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't find by ID, %v\n", err))
+		return
+	}
+
+	//	Retorna o Status 200 e o JSON da struct buscada
+	responses.JSON(w, http.StatusOK, moduloGotten)
+}
+
+/*  =========================
 	FUNCAO LISTAR TODOS MODULO
 =========================  */
 
@@ -102,4 +141,131 @@ func (server *Server) GetAllModulo(w http.ResponseWriter, r *http.Request) {
 
 	//	Retorna o Status 200 e o JSON da struct buscada
 	responses.JSON(w, http.StatusOK, allModulo)
+}
+
+/*  =========================
+	FUNCAO EDITAR MODULO
+=========================  */
+
+func (server *Server) UpdateModulo(w http.ResponseWriter, r *http.Request) {
+
+	//	Autorizacao de Modulo
+	err := config.AuthMod(w, r, 11003)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
+		return
+	}
+
+	//	Vars retorna as variaveis de rota
+	vars := mux.Vars(r)
+
+	//	codModulo armazena a chave primaria da tabela modulo
+	codModulo, err := strconv.ParseUint(vars["cod_modulo"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] it couldn't read the 'body', %v\n", err))
+		return
+	}
+
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	modulo := models.Modulo{}
+	logModulo := models.Log{}
+
+	err = json.Unmarshal(body, &modulo)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
+		return
+	}
+
+	err = validation.Validator.Struct(modulo)
+	if err != nil {
+		log.Printf("[WARN] invalid information, because, %v\n", fmt.Errorf("[FATAL] validation error!, %v\n", err))
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primeario nome_tabela, operacao, id_usuario)
+	err = logModulo.LogModulo(server.DB, uint32(codModulo), "modulo", "u", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
+	//	updateModulo recebe o nova modulo, a que foi alterada
+	updateModulo, err := modulo.UpdateModulo(server.DB, uint32(codModulo))
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't update in database , %v\n", formattedError))
+		return
+	}
+
+	//	Retorna o Status 200 e o JSON da struct alterada
+	responses.JSON(w, http.StatusOK, updateModulo)
+}
+
+/*  =========================
+	FUNCAO DELETAR MODULO
+=========================  */
+
+func (server *Server) DeleteModulo(w http.ResponseWriter, r *http.Request) {
+
+	//	Autorizacao de Modulo, apenas quem tem permicao de edit pode deletar
+	err := config.AuthMod(w, r, 11003)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
+		return
+	}
+
+	// Vars retorna as variaveis de rota
+	vars := mux.Vars(r)
+
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	modulo := models.Modulo{}
+	logModulo := models.Log{}
+
+	//	codModulo armazena a chave primaria da tabela modulo
+	codModulo, err := strconv.ParseUint(vars["cod_modulo"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
+		return
+	}
+
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primeario nome_tabela, operacao, id_usuario)
+	err = logModulo.LogModulo(server.DB, uint32(codModulo), "modulo", "d", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
+	// 	Para o caso da funcao 'delete' apenas o erro nos eh necessario
+	err = modulo.DeleteModulo(server.DB, uint32(codModulo))
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't delete in database , %v\n", formattedError))
+		return
+	}
+
+	w.Header().Set("Entity", fmt.Sprintf("%d", codModulo))
+
+	//	Retorna o Status 204, indicando que a informacao foi deletada
+	responses.JSON(w, http.StatusNoContent, "")
 }
