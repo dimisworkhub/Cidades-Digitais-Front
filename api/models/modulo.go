27c2e7ec1ff27084c4846e0bfd8ec587a97cb5e5
alexandreps1123 <alexandre.ps1123@gmail.com>
2021-01-21 11:06:05 -0300

    Criado as funçoes de GetByID, Update e Delete.

--- api/models/modulo.go
+++ api/models/modulo.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 /*  =========================
 	STRUCT MODULO
@@ -30,6 +32,21 @@ func (modulo *Modulo) SaveModulo(db *gorm.DB) (*Modulo, error) {
 }
 
 /*  =========================
+	FUNCAO LISTAR MODULO POR ID
+=========================  */
+
+func (modulo *Modulo) FindModuloByID(db *gorm.DB, codModulo uint32) (*Modulo, error) {
+
+	//	Busca um elemento no banco de dados a partir de sua chave primaria
+	err := db.Debug().Model(Modulo{}).Where("cod_modulo = ?", codModulo).Take(&modulo).Error
+	if err != nil {
+		return &Modulo{}, err
+	}
+
+	return modulo, err
+}
+
+/*  =========================
 	FUNCAO LISTAR TODOS MODULOS
 =========================  */
 
@@ -43,4 +60,38 @@ func (modulo *Modulo) FindAllModulo(db *gorm.DB) (*[]Modulo, error) {
 	}
 
 	return &allModulo, err
+}
+
+/*  =========================
+	FUNCAO EDITAR MODULO
+=========================  */
+
+func (modulo *Modulo) UpdateModulo(db *gorm.DB, codModulo uint32) (*Modulo, error) {
+
+	//	Permite a atualizacao dos campos indicados
+	db = db.Debug().Exec("UPDATE modulo SET categoria_1 = ?, ccategoria_2 = ?, categoria_3 = ?, descricao = ? WHERE cod_modulo = ?", modulo.Categoria_1, modulo.Categoria_2, modulo.Categoria_3, modulo.Descricao, codModulo)
+	if db.Error != nil {
+		return &Modulo{}, db.Error
+	}
+
+	//	Busca um elemento no banco de dados a partir de sua chave primaria
+	err := db.Debug().Model(&Modulo{}).Where("cod_modulo = ?", codModulo).Take(&modulo).Error
+	if err != nil {
+		return &Modulo{}, err
+	}
+
+	// retorna o elemento que foi alterado
+	return modulo, err
+}
+
+/*  =========================
+	FUNCAO DELETAR MODULO
+=========================  */
+
+func (modulo *Modulo) DeleteModulo(db *gorm.DB, codModulo uint32) error {
+
+	//	Deleta um elemento contido no banco de dados a partir de sua chave primaria
+	db.Debug().Model(&Itens{}).Where("cod_modulo = ?", codModulo).Take(&Modulo{}).Delete(&Modulo{})
+
+	return db.Error
 }
